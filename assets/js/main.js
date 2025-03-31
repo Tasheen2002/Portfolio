0b669b2e9a0aa4beaf677da21cc0d86354a5a298
Tasheen2002 <darshikatasheen99@gmail.com>
2025-03-31 10:05:50 +0530

    completed header and navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+if (navToggle) {
+  navToggle.addEventListener("click", (event) => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+if (navClose) {
+  navClose.addEventListener("click", (event) => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll(".nav_link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SWIPER PROJECTS ===============*/
 
 /*=============== SWIPER TESTIMONIAL ===============*/
