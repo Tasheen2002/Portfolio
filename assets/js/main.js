dc89794aecf5d393eaffb2bc7d4bd9ebd1fa85fc
Tasheen2002 <darshikatasheen99@gmail.com>
2025-04-06 18:15:35 +0530

    scroll bar and related this created

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,6 +89,27 @@ const sendEmail = (e) => {
 };
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav_menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
